bfe602f9d401dcb57091b3250d19d19123baade3
suda <skentaro36@gmail.com>
2014-12-31 00:36:43 +0900

    ゲーム制作編まで

---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Spaceship : MonoBehaviour {
+	public float speed;
+	public float shotDelay;
+
+	public GameObject bullet;
+	public GameObject explosion;
+
+	public bool canShot; 
+
+	private Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	public void Explosion () {
+		Instantiate(explosion, transform.position, transform.rotation);
+	}
+
+	public void Shot (Transform origin) {
+		Instantiate(bullet, origin.position, origin.rotation);
+	}
+
+	public Animator GetAnimator () {
+		return animator;
+	}
+}
